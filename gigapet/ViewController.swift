d97998b5da2ccefbb5fdee1faba8293b90537066
bromodachi <curaga1@gmail.com>
2016-06-12 14:24:35 +0900

    Initial Commit

---
+++ gigapet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gigapet
+//
+//  Created by hackintosh on 6/12/16.
+//  Copyright © 2016 bromodachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
